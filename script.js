b03646517c5e872209b44113d5ef3826a266cd65
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2021-06-08 12:27:39 +0530

    UI change

--- script.js
+++ script.js
@@ -10,83 +10,105 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
-let check = 0
-let userData = {}
-let username = ''
-let userinfo = {}
-let userstatus = {}
-let userrating = {}
+// let check = 0
+// let userData = {}
+// let username = ''
+// let userinfo = {}
+// let userstatus = {}
+// let userrating = {}
 
 app.get('/', (req,res) => {
     res.render('index')
 })
 
 
-app.post('/search', async(req,res) => {
-    const fetchInfo = async () => {
-        username = req.body.user
-        username = username.trim()
-        userData['username'] = username
+// app.post('/search', async(req,res) => {
+//     const fetchInfo = async () => {
+//         username = req.body.user
+//         username = username.trim()
+//         userData['username'] = username
+//         try {
+//             let link = 'https://codeforces.com/api/user.info?handles='
+//             link += username 
+//             userinfo = await axios.get(link)
+//             return userinfo.data
+//         }
+//         catch {
+//             console.log('error in info')
+//             res.render('profile_not_found', {username})
+//             check++
+//         }
+//     }
+    
+//     const fetchStatus = async() => {
+//         try {
+//             let link = 'https://codeforces.com/api/user.status?handle='
+//             link += username 
+//             userstatus = await axios.get(link)
+//             return userstatus.data
+//         } 
+//         catch(error) {
+//             console.error('error in status')
+//         }
+//     }  
+//     const fetchRating = async() => {
+//         try {
+//             let link = 'https://codeforces.com/api/user.rating?handle='
+//             link += username 
+//             userrating = await axios.get(link)
+//             return userrating.data
+//         }
+//         catch(error) {
+//             console.log('error in rating')
+//         }
+//     } 
+//     const fetchBlogEntries = async() => {
+//         try {
+//             let link = 'https://codeforces.com/api/user.blogEntries?handle='
+//             link += username
+//             userblogEntries = await axios.get(link)
+//             return userblogEntries.data
+//         }
+//         catch(error) {
+//             console.log('error in blog entries')
+//         }
+//     }
+//     userData['userinfo'] = await fetchInfo()
+//     userData['userstatus'] = await fetchStatus()
+//     userData['userrating'] = await fetchRating()
+//     userData['userblogEntries'] = await fetchBlogEntries()
+//     if (check === 0)
+//         res.redirect(`/search?user=${username}`)
+//     //console.log(userData) 
+// })
+
+
+app.get('/methods', (req,res) => {
+    res.render('methods_home_page')
+})
+
+
+app.get('/methods/:methodName', (req,res) => {
+    const methodName = req.params.methodName
+    res.render (`methods/${methodName}/${methodName}Home`, {methodName})
+})
+
+app.post('/methods/:methodName', (req,res) => {
+    let id = req.body.id
+    const methodName = req.body.methodName
+    res.redirect(`/methods/${methodName}/id=${id}`)
+    app.get(`/methods/${methodName}/id=${id}`, async(req,res) => {
         try {
-            let link = 'https://codeforces.com/api/user.info?handles='
-            link += username 
-            userinfo = await axios.get(link)
-            return userinfo.data
+            id = Number(id)
+            const linkView = await axios.get(`https://codeforces.com/api/blogEntry.view?blogEntryId=${id}`)
+            const linkComments = await axios.get(`https://codeforces.com/api/blogEntry.comments?blogEntryId=${id}`)
+            res.render(`methods/${methodName}/${methodName}Success`,{id, linkView, linkComments})
         }
         catch {
-            console.log('error in info')
-            res.render('profile_not_found', {username})
-            check++
+            res.render(`methods/${methodName}/${methodName}Failure`, {id})
         }
-    }
-    
-    const fetchStatus = async() => {
-        try {
-            let link = 'https://codeforces.com/api/user.status?handle='
-            link += username 
-            userstatus = await axios.get(link)
-            return userstatus.data
-        } 
-        catch(error) {
-            console.error('error in status')
-        }
-    }  
-    const fetchRating = async() => {
-        try {
-            let link = 'https://codeforces.com/api/user.rating?handle='
-            link += username 
-            userrating = await axios.get(link)
-            return userrating.data
-        }
-        catch(error) {
-            console.log('error in rating')
-        }
-    } 
-    const fetchBlogEntries = async() => {
-        try {
-            let link = 'https://codeforces.com/api/user.blogEntries?handle='
-            link += username
-            userblogEntries = await axios.get(link)
-            return userblogEntries.data
-        }
-        catch(error) {
-            console.log('error in blog entries')
-        }
-    }
-    userData['userinfo'] = await fetchInfo()
-    userData['userstatus'] = await fetchStatus()
-    userData['userrating'] = await fetchRating()
-    userData['userblogEntries'] = await fetchBlogEntries()
-    if (check === 0)
-        res.redirect(`/search?user=${username}`)
-    //console.log(userData) 
+    })
 })
-
-
-app.get('/search', (req,res) => {
-    res.render('userProfile', {userData})
-})
-
 
 app.listen(8080, () => {
     console.log('LISTENING ON PORT 8080')
