0b014d6d9f57788704cebb96bf47981d05e69510
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2021-06-10 15:38:28 +0530

    recentActions's methods added

--- script.js
+++ script.js
@@ -16,6 +16,16 @@ let filler = null
 let id = null
 let countFillercheck = false
 
+const escapeJson = function (json) {
+    if (typeof json !== 'string') {
+	return json;
+    }
+    return json.replace(/</g, '\\u003c')
+	       .replace(/>/g, '\\u003e')
+	       .replace(/&/g, '\\u0026');
+};
+
+
 
 app.get('/', (req,res) => {
     res.render('index')
@@ -28,13 +38,17 @@ app.get('/methods', (req,res) => {
 
 app.get('/methods/:methodName', async(req,res) => {
     const methodName = req.params.methodName
-    if (methodName === 'blogEntry')
+    if (methodName === 'blogEntry' || methodName === 'recentActions') {
         res.render(`methods/method_form`, {methodName})
+    }
     else if (methodName === 'contest') {
         let [linkcontest_listT,linkcontest_listF] = await Promise.all([axios.get(`https://codeforces.com/api/contest.list?gym=true`),axios.get(`https://codeforces.com/api/contest.list?gym=false`)])
+        linkcontest_listT = JSON.stringify(linkcontest_listT.data)
+        linkcontest_listT = escapeJson(linkcontest_listT)
+        linkcontest_listF = JSON.stringify(linkcontest_listF.data)
+        linkcontest_listF = escapeJson(linkcontest_listF)
         linkData.linkcontest_listT = linkcontest_listT
         linkData.linkcontest_listF = linkcontest_listF
-        //console.log(linkData.linkcontest_listF.data)
         res.render('methods/method_home', {linkData,methodName,check})
     }
     else if (methodName === 'problemset') {
@@ -70,20 +84,32 @@ app.get('/methods/:methodName/:id', async(req,res) => {
         else {
             id = req.params.id.substring(5)
         }
-        console.log(id)
+        //console.log(id)
         //console.log(filler)
     }
     if (methodName === 'blogEntry') {
         let [linkblogEntry_comments,linkblogEntry_view] = await Promise.all([axios.get(`https://codeforces.com/api/blogEntry.comments?blogEntryId=${id}`),axios.get(`https://codeforces.com/api/blogEntry.view?blogEntryId=${id}`)])
+        linkblogEntry_comments = JSON.stringify(linkblogEntry_comments.data)
+        linkblogEntry_comments = escapeJson(linkblogEntry_comments)
         linkData.linkblogEntry_comments = linkblogEntry_comments
+        linkblogEntry_view = JSON.stringify(linkblogEntry_view.data)
+        linkblogEntry_view = escapeJson(linkblogEntry_view)
         linkData.linkblogEntry_view = linkblogEntry_view
         res.render('methods/method_home', {linkData,id,methodName})
     }
     else if (methodName === 'contest') {
         let [linkcontest_hacks,linkcontest_ratingChanges,linkcontest_standings,linkcontest_status] = await Promise.all([axios.get(`https://codeforces.com/api/contest.hacks?contestId=${id}`),axios.get(`https://codeforces.com/api/contest.ratingChanges?contestId=${id}`),axios.get(`https://codeforces.com/api/contest.standings?contestId=${id}`),axios.get(`https://codeforces.com/api/contest.status?contestId=${id}`)])
+        linkcontest_hacks = JSON.stringify(linkcontest_hacks.data)
+        linkcontest_hacks = escapeJson(linkcontest_hacks)
         linkData.linkcontest_hacks = linkcontest_hacks
+        linkcontest_ratingChanges = JSON.stringify(linkcontest_ratingChanges.data)
+        linkcontest_ratingChanges = escapeJson(linkcontest_ratingChanges)
         linkData.linkcontest_ratingChanges = linkcontest_ratingChanges
+        linkcontest_standings = JSON.stringify(linkcontest_standings.data)
+        linkcontest_standings = escapeJson(linkcontest_standings)
         linkData.linkcontest_standings = linkcontest_standings
+        linkcontest_status = JSON.stringify(linkcontest_status.data)
+        linkcontest_status = escapeJson(linkcontest_status)
         linkData.linkcontest_status = linkcontest_status
         //console.log(linkData.linkcontest_ratingChanges.data)
         check = 1
@@ -92,14 +118,18 @@ app.get('/methods/:methodName/:id', async(req,res) => {
     }
     else if (methodName === 'problemset') {
         if (filler === 'countFiller') {
-            console.log('llll')
+            //console.log('llll')
             let linkproblemset_recentStatus = await axios.get(`https://codeforces.com/api/problemset.recentStatus?count=${id}`)
+            linkproblemset_recentStatus = JSON.stringify(linkproblemset_recentStatus.data)
+            linkproblemset_recentStatus = escapeJson(linkproblemset_recentStatus)
             linkData.linkproblemset_recentStatus = linkproblemset_recentStatus
             countFillercheck = true
         }
         else {
-            console.log('kkk')
+            //console.log('kkk')
             let linkproblemset_problems = await axios.get(`https://codeforces.com/api/problemset.problems?tags=${id}`)
+            linkproblemset_problems = JSON.stringify(linkproblemset_problems.data)
+            linkproblemset_problems = escapeJson(linkproblemset_problems)
             linkData.linkproblemset_problems = linkproblemset_problems
         }
         check = 1
@@ -107,14 +137,25 @@ app.get('/methods/:methodName/:id', async(req,res) => {
         check = 0
         countFillercheck = false
     }
+    else if (methodName === 'recentActions') {
+        id = req.params.id.substring(9)
+        id = Number(id)
+        let linkrecentActions = await axios.get(`https://codeforces.com/api/recentActions?maxCount=${id}`)
+        linkrecentActions = JSON.stringify(linkrecentActions.data)
+        linkrecentActions = escapeJson(linkrecentActions)
+        linkData.linkrecentActions = linkrecentActions
+        res.render('methods/method_home', {linkData,methodName,id})
+    }
 })
 
 
 app.post('/methods/:methodName',  (req,res) => {
     const methodName = req.params.methodName
     id = req.body.id
-    if (methodName === 'blogEntry' || methodName === 'contest')
+    if (methodName === 'blogEntry' || methodName === 'contest') {
         id = Number(id)
+        res.redirect(`/methods/${req.params.methodName}/id=${id}`)
+    }
     if (methodName === 'problemset') {
         if (filler === 'countFiller') {
             id = Number(id)
@@ -125,8 +166,8 @@ app.post('/methods/:methodName',  (req,res) => {
         }
     }
     //console.log(req.params.methodName)
-    else {
-        res.redirect(`/methods/${req.params.methodName}/id=${id}`)
+    else if (methodName === 'recentActions') {
+        res.redirect(`/methods/${req.params.methodName}/maxCount=${id}`)
     }
 })
 
